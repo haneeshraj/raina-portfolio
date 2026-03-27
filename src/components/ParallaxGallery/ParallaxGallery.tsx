9a4f6c197e65ef20956c06605211f75bdb58bcad
Haneesh Raj Banisetti <haneeshraj.banisetti@gmail.com>
2026-03-27 05:25:30 -0400

    more fixes

--- src/components/ParallaxGallery/ParallaxGallery.tsx
+++ src/components/ParallaxGallery/ParallaxGallery.tsx
@@ -38,6 +38,12 @@ export default function ParallaxGallery({
 }: ParallaxGalleryProps) {
   const gallery = useRef(null);
   const [dimension, setDimension] = useState({ width: 0, height: 0 });
+  const [multipliers, setMultipliers] = useState({
+    y: 2,
+    y2: 3.3,
+    y3: 1.25,
+    y4: 3,
+  });
 
   const { scrollYProgress } = useScroll({
     target: gallery,
@@ -45,10 +51,22 @@ export default function ParallaxGallery({
   });
 
   const { height } = dimension;
-  const y = useTransform(scrollYProgress, [0, 1], [0, height * 2]);
-  const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3]);
-  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25]);
-  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3]);
+  const y = useTransform(scrollYProgress, [0, 1], [0, height * multipliers.y]);
+  const y2 = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0, height * multipliers.y2],
+  );
+  const y3 = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0, height * multipliers.y3],
+  );
+  const y4 = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0, height * multipliers.y4],
+  );
 
   useEffect(() => {
     const lenis = new Lenis();
@@ -60,6 +78,14 @@ export default function ParallaxGallery({
 
     const resize = () => {
       setDimension({ width: window.innerWidth, height: window.innerHeight });
+
+      if (window.innerWidth <= 480) {
+        setMultipliers({ y: 1, y2: 1.5, y3: 1, y4: 1 });
+      } else if (window.innerWidth <= 1024) {
+        setMultipliers({ y: 1.5, y2: 2, y3: 1, y4: 1.8 });
+      } else {
+        setMultipliers({ y: 2, y2: 3.3, y3: 1.25, y4: 3 });
+      }
     };
 
     window.addEventListener("resize", resize);
